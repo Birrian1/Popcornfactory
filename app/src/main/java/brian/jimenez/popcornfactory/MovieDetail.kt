e6b96c6080cfa08d6eb216e63aaf1d75cf8aa2c8
DESKTOP-UCNGISC\BCJL1 <91810728+Birrian1@users.noreply.github.com>
2023-02-15 19:09:43 -0700

    Initial commit

---
+++ app/src/main/java/brian/jimenez/popcornfactory/MovieDetail.kt
@@ -0,0 +1,11 @@
+package brian.jimenez.popcornfactory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MovieDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_detail)
+    }
+}
---
+++ app/src/main/java/brian/jimenez/popcornfactory/Pelicula.kt
@@ -0,0 +1,5 @@
+package brian.jimenez.popcornfactory
+
+import android.media.Image
+
+data class Pelicula(var titulo:String, var image: Int, var header:Int, var sinopsis:String)
